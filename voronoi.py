546666748522632fe8eec2ccaf50f723bf696f96
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-12-07 14:20:48 -0500

    Used the quad tree impl from the research paper

--- voronoi.py
+++ voronoi.py
@@ -1,67 +1,72 @@
+import math
 import cv2
 import numpy as np
 import queue
 
-# ne = img[0:50, 0:50]
-# cv2.circle(img, (100, 100), 55, (250, 0, 0))
-# cv2.imshow("image", ne)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
+def __error__(img):
+    """Compute the error """
+    avg_rgb = __average_rgb__(img)
+    d2 = np.sum(np.power((img - avg_rgb) / 255, 2), axis=2)
+    err = d2.max(axis=1).max(axis=0).max()
+    return err
 
 
-class Cell(object):
+def __average_rgb__(img):
+    """Get average rgb value """
+    r = np.average(img[:, :, 0])
+    g = np.average(img[:, :, 1])
+    b = np.average(img[:, :, 2])
+    return np.array([r, g, b])
 
-    """A cell is a block of the image with some upper bound on the size"""
 
-    def __init__(self, start_x, start_y, width, height, image):
-        """Can create a rectangular area of of wxh that contains
-        the pixels specified.
+class Quad:
 
-        :start_x, start_y: the starting (top left corner of the rectangle)
-
+    def __init__(self, img, x=0, y=0, min_area=64, error_rate=0.3, is_root=False):
+        """Quad constructor 
+        Keyword arguments:
+        img -- the input image
+        x   -- the upper-left x position of this image
+        y   -- the upper-left y position of this image
         """
-        self.start_x = start_x
-        self.start_y = start_y
-        self.width = width
-        self.height = height
-        self.children = []
-        self.img = image
-
-
-class QuadTree:
-
-    """QuadTree -- similar to binary tree but with four children"""
-
-    def __init__(self, size_limit,  cell, error_rate=0.5, is_root=False):
-        """ Generates a quad decomposition of cells
-
-        :size_limit: the min size of a cell to recurse to
-        :image: image from opencv
-        """
-        self.size_limit = size_limit
-        self.cell = cell
+        self.img = img
+        self.height = img.shape[0]
+        self.width = img.shape[1]
+        self.x = x
+        self.y = y
+        self.min_area = min_area
         self.error_rate = error_rate
         self.is_root = is_root
-        self.is_leaf = self.is_leaf()
-        self.leaves = []
-        if is_root:
-            self.leaves = self.generate()
+        self.leaf = self.is_leaf()
+        self.children = None
+        # If this node is root then generate the quadtree
+        if is_root is True:
+            self.leaves = self.generate_tree()
 
-    def generate(self):
+    def generate_tree(self):
+        """Generate the quadtree 
+        Note: it is legal to call only when the tree node is root
+        """
+        # Verify the caller is root
+        if self.is_root is False:
+            raise Exception('The caller is not a root')
 
-        width = self.cell.width / 2
-        height = self.cell.height / 2
-
-        leaves = []
+        leaves = list()
+        # Create a queue and push root
         qu = queue.Queue()
         qu.put(self)
 
+        # Pop all the elements out the queue in the same level and
+        # put inside children with level - 1 if this node has children
+        # until the queue is empty
         while qu.empty() is False:
             q_size = qu.qsize()
+            # For all the elements in the same level
             for _ in range(q_size):
                 child = qu.get()
-                if child.is_leaf is False:
-                    tl, tr, bl, br = child.split()
+                # If the node is a leaf then do nothing and appends it to the leaves list
+                if child.leaf is False:
+                    tl, tr, bl, br = child.__subdivide__()
                     qu.put(tl)
                     qu.put(tr)
                     qu.put(bl)
@@ -71,63 +76,55 @@ class QuadTree:
 
         return leaves
 
+    def __subdivide__(self):
+        """Subdivide this quadrature tree and produce four child """
+        # Compute width and height of the child region
+        child_width = math.ceil(self.width / 2)
+        child_height = math.ceil(self.height / 2)
+        # Generate four sub-quadtree nodes
+        top_left = Quad(self.img[:child_height, :child_width, :], x=self.x,
+                        y=self.y, min_area=self.min_area, error_rate=self.error_rate)
+        top_right = Quad(self.img[:child_height, child_width:, :], x=self.x,
+                         y=self.y + child_width, min_area=self.min_area, error_rate=self.error_rate)
+        bottom_left = Quad(self.img[child_height:, :child_width, :], x=self.x +
+                           child_height, y=self.y, min_area=self.min_area, error_rate=self.error_rate)
+        bottom_right = Quad(self.img[child_height:, child_width:, :], x=self.x + child_height,
+                            y=self.y + child_width, min_area=self.min_area, error_rate=self.error_rate)
+        # Mark all children
+        self.children = [top_left, top_right, bottom_left, bottom_right]
+        return top_left, top_right, bottom_left, bottom_right
+
     def is_leaf(self):
         """Check whether this node is a leaf """
-        return self.cell.height * self.cell.width <= self.size_limit or __error__(self.cell.img) <= self.error_rate
-
-    def split(self):
-        """
-         Recursively splits the image into rectangles
-
-        :returns: the split 4 chidren
-        """
-        child_width = math.ceil(self.cell.width / 2)
-        child_height = math.ceil(self.cell.height / 2)
-        top_left = Quad(size_limit, Cell(self.cell.start_x, self.cell.start_y, child_height, child_width, self.cell.img[:child_height, :child_width, :]))
-        top_right=Quad(size_limit, Cell(self.cell.start_x, self.cell.start_y+child_width, child_height, child_width, self.cell.img[:child_height, :child_width, :]))
-
-        bottom_left=top_right=Quad(size_limit, Cell(self.cell.start_x+child_height, self.cell.start_y, child_height, child_width, self.cell.img[:child_height, :child_width, :]))
-        bottom_right=Quad(size_limit, Cell(self.cell.start_x+child_height, self.cell.start_y+child_width, child_height, child_width, self.cell.img[:child_height, :child_width, :]))
-        self.children=[top_left, top_right, bottom_left, bottom_right]
-        return top_left, top_right, bottom_left, bottom_right
+        if self.height * self.width <= self.min_area or __error__(self.img) <= self.error_rate:
+            return True
+        else:
+            return False
 
     def generate_seeds(self):
         """Generate seeds """
-        point_x=np.zeros(len(self.leaves))
-        point_y=np.zeros(len(self.leaves))
+        point_x = np.zeros(len(self.leaves))
+        point_y = np.zeros(len(self.leaves))
 
         for i in range(len(self.leaves)):
-            block=self.leaves.pop()
-            x=block.cell.start_x
-            y=block.cell.start_y
-            w=block.cell.width
-            h=block.cell.height
-            point_y[i]=x + w / 2
-            point_x[i]=y + h / 2
+            block = self.leaves.pop()
+            x = block.x
+            y = block.y
+            w = block.width
+            h = block.height
+            point_y[i] = x + w / 2
+            point_x[i] = y + h / 2
 
         return point_x, point_y
 
 
-def __error__(img):
-    """Compute the error """
-    avg_rgb=__average_rgb__(img)
-    d2=np.sum(np.power((img - avg_rgb) / 255, 2), axis=2)
-    err=d2.max(axis=1).max(axis=0).max()
-    return err
+img = cv2.imread('flower.jpg')
+q = Quad(img, error_rate=0.3, is_root=True)
 
-def __average_rgb__(img):
-    """Get average rgb value """
-    r=np.average(img[:, :, 0])
-    g=np.average(img[:, :, 1])
-    b=np.average(img[:, :, 2])
-    return np.array([r, g, b])
+xs, ys = q.generate_seeds()
+for i in range(min(len(xs), len(ys))):
+    cv2.circle(img, (int(xs[i]), int(ys[i])), 2, (250, 0, 0))
 
-
-
-
-img=cv2.imread('gabe.jpg')
-quad = QuadTree(64, Cell(0, 0, img.shape[0], img.shape[1], img),is_root=True)
-print(quad.leaves)
-x,y = quad.generate_seeds()
-print(x,y)
-
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
