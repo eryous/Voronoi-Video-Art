d51e4788555dc6730066ea0ccdda0f855b55a73c
Youssef <eryous.ye@yahoo.com>
2019-12-08 02:09:17 -0500

    updated database, fixed the html

--- application/web/views.py
+++ application/web/views.py
@@ -7,35 +7,42 @@ import cv2
 
 def home(request):
 
-    cur_vid = Video.objects.all()[0]
+    cur_vid = Video.objects.last()
     form = VideoForm(request.POST or None, request.FILES or None)
     print(cur_vid)
     vid_file_path = "../media/videos/IMG_7028.mp4"
     print(form.errors)
+    if request.method == 'POST':
+        vid_file_path = cur_vid.videofile
+        if form.is_valid():
+            # if (cur_vid):
+            #     vid_file_path = cur_vid.videofile
+            # else:
+            #     data = form.cleaned_data
+            #     file_name = data["name"]
+            #     vid_file_path = "../media/videos/" + file_name
 
-    vid_file_path = cur_vid.videofile
-    if form.is_valid():
-        # if (cur_vid):
-            # vid_file_path = cur_vid.videofile
-        # else:
-            # data = form.cleaned_data
-            # file_name = data["name"]
-            # vid_file_path = "../media/videos/" + file_name
+            print("IN HOME", vid_file_path)
+            data = form.cleaned_data
+            print(data)
+            fps = data["fps"]
+            name = data["name"]
+            error_rate = data["error_rate"]
+            form.save()
 
-        print("IN HOME", vid_file_path)
-        data = form.cleaned_data
-        print(data)
-        fps = data["fps"]
-        name = data["name"]
-        error_rate = data["error_rate"]
-        form.save()
+            capture = cv2.VideoCapture(vid_file_path.url[1:])
 
-        capture = cv2.VideoCapture(vid_file_path.url[1:])
-
-        video_to_voronoi(name, ".."+vid_file_path.url,
-                         "media/videos/", fps, error_rate, capture)
-    context = {'videofile': "media/videos/voronoi_"+name+".mp4",
-               'form': form,
-               }
-
+            video_to_voronoi(name, ".."+vid_file_path.url,
+                            "media/videos/", fps, error_rate, capture)
+            context = {'videofile': "media/videos/voronoi_"+name+".mp4",
+                    'form': form,
+                    }
+        else:
+            context = {'videofile': "",
+                    'form': form,
+                    }
+    else:
+        context = {'videofile': "",
+                'form': form,
+                }
     return render(request, 'home.html', context)
