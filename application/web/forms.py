8e0fafe03a89efb91a5e3ae7c3381a9601630338
Youssef <eryous.ye@yahoo.com>
2020-07-05 00:12:20 -0400

    cleaning up codebase

--- application/web/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.db import models
-from .models import *
-
-
-class VideoForm(forms.ModelForm):
-    class Meta:
-        model = Video
-        fields = ["name", "videofile", "fps", "error_rate"]
